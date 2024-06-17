61ac4ded4e88449b30830fb271a4901458aa250d
Atoposiii <zhenglei_zz@163.com>
2024-06-17 14:21:14 +0800

    新增采集信息上传到飞书在线文档上

---
+++ Common/generate_report.py
@@ -0,0 +1,221 @@
+import lark_oapi as lark
+import requests
+import json
+from lark_oapi.api.sheets.v3 import *
+from datetime import datetime
+
+# SDK 使用说明: https://github.com/larksuite/oapi-sdk-python#readme
+
+def generate_report(report_data):
+    # 应用ID和密钥
+    app_id = "cli_a6cded67377d900d"
+    app_secret = "J8NAOwwb36KVnydUtq1SycsKa8eXHzxS"
+    # 创建client
+    client = lark.Client.builder() \
+        .app_id(app_id) \
+        .app_secret(app_secret) \
+        .log_level(lark.LogLevel.DEBUG) \
+        .build()
+
+    # 获取应用访问令牌
+    app_access_token = get_app_access_token(app_id, app_secret)
+    # 用户访问令牌
+    user_access_token = "u-fR5Zg.ILxcsqLAPYLa5mVyhkkcYl4kDxg0w0klE00CtF"
+    # 创建电子表格
+    # create_sheet(app_id, app_secret)
+    # 获取创建的表格token
+    # spreadsheet_token = response.data.spreadsheet.spreadsheet_token
+    # 获取工作表
+    sheet_info = get_sheet(client=client)
+    titles = [sheet['title'] for sheet in sheet_info]
+    title = datetime.today().strftime('%Y-%m-%d')
+    spreadsheet_token = "GCoLsuawvhPxfdtypTNcATEqnKd"
+    # 如果sheet未创建，首先要创建
+    sheet_id = None
+    if title not in titles:
+        sheet_id = sheets_update(spreadsheet_token, app_access_token, title)
+    else:
+        sheet_id = find_sheet_id_by_title(sheet_info, title)
+    # 获取sheetId
+    # sheet_id = get_sheet_id(spreadsheet_token, app_access_token)
+    if not sheet_id:
+        lark.logger.error("获取sheetId失败")
+        return
+    # 添加表头信息
+    add_values_to_sheet(spreadsheet_token, app_access_token, f"{sheet_id}!A1:Q1",
+                        [["用例名", "代码行", "函数个数", "gtest行覆盖",
+                          "gtest函数覆盖", "fuzzer行覆盖", "fuzzer函数覆盖",
+                          "gtest行覆盖率", "gtest函数覆盖率", "fuzzer行覆盖率", "fuzzer函数覆盖率",
+                          "行覆盖提升率", "函数覆盖提升率", "bug总数", "bug类型", "用例bug数",
+                          "源码bug数"]])
+    # 添加数据信息
+    append_to_sheet(spreadsheet_token, app_access_token, f"{sheet_id}!A1:Q1",
+                    [report_data])
+
+def find_sheet_id_by_title(sheets, title_to_find):
+    # 遍历sheets列表
+    for sheet in sheets:
+        # 检查当前sheet的title是否与要查询的title匹配
+        if sheet['title'] == title_to_find:
+            # 如果匹配，返回对应的sheet_id
+            return sheet['sheet_id']
+    return None
+
+def get_sheet(client):
+    # 构造请求对象
+    request: QuerySpreadsheetSheetRequest = QuerySpreadsheetSheetRequest.builder() \
+        .spreadsheet_token("GCoLsuawvhPxfdtypTNcATEqnKd") \
+        .build()
+
+    # 发起请求
+    response: QuerySpreadsheetSheetResponse = client.sheets.v3.spreadsheet_sheet.query(request)
+
+    # 处理失败返回
+    if not response.success():
+        lark.logger.error(
+            f"client.sheets.v3.spreadsheet_sheet.query failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}")
+        return
+
+    # 处理业务结果
+    # lark.logger.info(lark.JSON.marshal(response.data, indent=4))
+    sheet_info = []
+    for sheet in response.data.sheets:
+        sheet_info.append(
+            {
+                "sheet_id": sheet.sheet_id,
+                "title": sheet.title
+            }
+        )
+    return sheet_info
+
+
+def sheets_update(sheet_token, access_token, title):
+    url = f"https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{sheet_token}/sheets_batch_update"
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json; charset=utf-8"
+    }
+
+    body = {
+        "requests": [
+            {
+                "addSheet": {
+                    "properties": {
+                        "title": title,
+                    }
+                }
+            }
+        ]
+    }
+    response = requests.post(url, headers=headers, json=body)
+    return response.json()['data']['replies'][0]['addSheet']['properties']['sheetId']
+
+
+def create_sheet(app_id, app_secret, user_access_token):
+    # 创建client
+    client = lark.Client.builder() \
+        .app_id(app_id) \
+        .app_secret(app_secret) \
+        .enable_set_token(True) \
+        .log_level(lark.LogLevel.DEBUG) \
+        .build()
+    # 构造请求对象，创建新的电子表格
+    request = CreateSpreadsheetRequest.builder() \
+        .request_body(Spreadsheet.builder()
+                      .title("ABFerry Test Report")
+                      .build()) \
+        .build()
+    option = lark.RequestOption.builder().user_access_token(user_access_token).build()
+    response = client.sheets.v3.spreadsheet.create(request, option)
+
+    # 处理失败返回
+    if not response.success():
+        lark.logger.error(
+            f"client.sheets.v3.spreadsheet.create failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}")
+        return
+
+
+def get_sheet_id(spreadsheet_token, user_access_token):
+    url = f"https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{spreadsheet_token}/metainfo"
+    headers = {
+        "Authorization": f"Bearer {user_access_token}",
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200 or response.json().get("code") != 0:
+        lark.logger.error(
+            f"get_sheet_id failed, code: {response.json().get('code')}, msg: {response.json().get('msg')}")
+        return None
+    info = response.json()["data"]
+    print(info)
+    sheet_id = response.json()['data']['sheets'][0]['sheetId']
+    lark.logger.info(f"获取的sheetId: {sheet_id}")
+    return sheet_id
+
+
+def add_values_to_sheet(spreadsheet_token, access_token, range_address, values):
+    url = f"https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{spreadsheet_token}/values"
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    body = {
+        "valueRange": {
+            "range": range_address,
+            "values": values
+        }
+    }
+    response = requests.put(url, headers=headers, json=body)
+
+    if response.status_code != 200 or response.json().get("code") != 0:
+        lark.logger.error(
+            f"add_values_to_sheet failed, code: {response.json().get('code')}, msg: {response.json().get('msg')}")
+    else:
+        lark.logger.info("表头创建成功")
+
+
+def append_to_sheet(spreadsheet_token, access_token, range_address, values):
+    url = f"https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{spreadsheet_token}/values_append"
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    body = {
+        "valueRange": {
+            "range": range_address,
+            "values": values
+        }
+    }
+    response = requests.post(url, headers=headers, json=body)
+    if response.status_code != 200 or response.json().get("code") != 0:
+        lark.logger.error(
+            f"add_values_to_sheet failed, code: {response.json().get('code')}, msg: {response.json().get('msg')}")
+    else:
+        lark.logger.info("数据添加成功")
+
+
+def get_app_access_token(app_id, app_secret):
+    url = "https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal"
+    headers = {
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    body = {
+        "app_id": app_id,
+        "app_secret": app_secret
+    }
+    response = requests.post(url, headers=headers, json=body)
+    return response.json()['app_access_token']
+
+
+def print_http_request_details(response):
+    print("Status Code:", response.status_code)
+    print("Headers:", json.dumps(dict(response.headers), indent=4))
+    print("Response Body:", response.text)
+
+
+if __name__ == "__main__":
+    report_data = ['mini', 113, 10, 52, 6, 96, 9, '46.02%', '60.00%', '84.96%', '90.00%', '84.62%', '50.00%', 2,
+                   'FPE, heap-use-after-free', 0, 2]
+
+    generate_report(report_data)
--- tcase/test_generate_report.py
+++ tcase/test_generate_report.py
@@ -1,27 +1,25 @@
 import json
-import time
+import os
 import pytest
 import allure
 from pytest_assume.plugin import assume
 from Common.config import Config
 from Common.core import HttpRequest
 from Common.core import ReportStyle
-
+from Common.generate_report import generate_report
 http_client = HttpRequest()
 
 test_cases = Config.get_yaml(Config.get_case_data_dir() + Config.get_case_data_file())
 
-report_data = {}
+
+@pytest.fixture
+def report_data():
+    return {}
+
 
 @allure.feature("ABFerry测试")
 class TestFuzzResultInfo:
-    @allure.story("测试入口模块")
-    @allure.severity("blocker")
-    @allure.description("获取覆盖率")
-    @pytest.mark.parametrize("args", test_cases)
-    # @pytest.mark.skip(reason="暂时不用")
-    @allure.title("测试入口模块相关接口-获取测试用例覆盖率")
-    def test_instance_coverages(self, args):
+    def gather_coverage_info(self, args, report_data):
         instance_id = None
         with allure.step("step: 读取yaml文件，获取测试用例id"):
             project_info = Config.get_cpp_project_info()
@@ -30,7 +28,7 @@ class TestFuzzResultInfo:
                 report_data["用例名"] = name
                 if name == args["program_alias"]:
                     instance_id = project["instance_id"]
-                    ReportStyle.step("测试用例id: ", instance_id)
+                    # ReportStyle.step("测试用例id: ", instance_id)
                     break
 
         url = Config.TestEnv + "/api/structure/instanceCoverages"
@@ -50,11 +48,7 @@ class TestFuzzResultInfo:
         with allure.step("step: 统计覆盖率"):
             stats = {}
             for coverage in coverages:
-                # 多ut使用
-                # unit_name = coverage["unitName"]
-                # if unit_name not in stats:
                 if coverage["projectId"] != "":
-                    # stats[unit_name]
                     stats = {
                         "gtest行覆盖Total": coverage["gtestLineTotal"],
                         "gtest行覆盖": coverage["gtestLineCovered"],
@@ -73,7 +67,7 @@ class TestFuzzResultInfo:
                         "fuzzer函数覆盖率": "{:.2f}%".format(
                             coverage["abferryFunctionCovered"] / coverage["abferryFunctionTotal"] * 100),
                     }
-            ReportStyle.step("覆盖率: ", stats)
+            # ReportStyle.step("覆盖率: ", stats)
 
             report_data["代码行"] = stats["gtest行覆盖Total"]
             report_data["函数个数"] = stats["gtest函数覆盖Total"]
@@ -88,28 +82,22 @@ class TestFuzzResultInfo:
         base_fuzzer_line_covered_rate = float(args.get("fuzzer_line_covered_rate").split("%")[0])
         base_fuzzer_function_covered_rate = float(args.get("fuzzer_function_covered_rate").split("%")[0])
 
-        report_data["行覆盖提升率"] = "{:.2f}%".format(((test_fuzzer_line_covered_rate - test_gtest_line_covered_rate) / test_gtest_line_covered_rate)*100)
-        report_data["函数覆盖提升率"] = "{:.2f}%".format(((test_fuzzer_function_covered_rate - test_gtest_function_covered_rate) / test_gtest_function_covered_rate)*100)
+        report_data["行覆盖提升率"] = "{:.2f}%".format(
+            ((test_fuzzer_line_covered_rate - test_gtest_line_covered_rate) / test_gtest_line_covered_rate) * 100)
+        report_data["函数覆盖提升率"] = "{:.2f}%".format(((
+                                                                      test_fuzzer_function_covered_rate - test_gtest_function_covered_rate) / test_gtest_function_covered_rate) * 100)
 
         with ((allure.step("step: 断言函数覆盖率是否正常"))):
             with assume:
-                #
                 assert test_fuzzer_function_covered_rate >= base_fuzzer_function_covered_rate \
-                or test_fuzzer_function_covered_rate > (base_fuzzer_function_covered_rate - 3)
+                       or test_fuzzer_function_covered_rate > (base_fuzzer_function_covered_rate - 3)
 
         with allure.step("step: 断言行覆盖率是否正常"):
             with assume:
-                #
                 assert test_fuzzer_line_covered_rate >= base_fuzzer_line_covered_rate \
-                or test_fuzzer_line_covered_rate > (base_fuzzer_line_covered_rate - 3)
+                       or test_fuzzer_line_covered_rate > (base_fuzzer_line_covered_rate - 3)
 
-    @allure.story("测试入口模块")
-    @allure.severity("blocker")
-    @allure.description("获取bug数")
-    @pytest.mark.parametrize("args", test_cases)
-    # @pytest.mark.skip(reason="暂时不用")
-    @allure.title("测试入口模块相关接口-获取测试用例bug数")
-    def test_instance_bugs(self, args):
+    def gather_bugs_info(self, args, report_data):
         instance_id = None
         with allure.step("step: 读取yaml文件，获取测试用例id"):
             project_info = Config.get_cpp_project_info()
@@ -140,31 +128,26 @@ class TestFuzzResultInfo:
                     "中断来源": bug["bugType"],
                     "bug来源": "用例" if bug["level"] == 0 else "源码"
                 }
-            ReportStyle.step("BUG列表: ", stats)
+            # ReportStyle.step("BUG列表: ", stats)
 
         test_bugs = []
         with allure.step("step: 统计BUG数量"):
             bug_count = len(bugs)
-            # 初始化字典来存储结果
             bug_stats = {
                 "bug总数": bug_count,
                 "bug类型": 0,
                 "用例bug数": 0,
                 "源码bug数": 0,
             }
-
-            # 初始化一个集合来存储不同的bug类型，以便计算bug的类型数
             bug_types = set()
 
             for item in bugs:
                 bug_name = item["bugName"]
-                source_line = item["sourceLine"]
+                source_line = item.get("sourceLine")
 
                 level = item["level"]
-                # 更新bug类型的集合
                 bug_types.add(bug_name)
 
-                # 根据level来更新用例的数量或源码的数量
                 if level == "0":
                     bug_stats["用例bug数"] += 1
                 elif level == "1":
@@ -176,24 +159,65 @@ class TestFuzzResultInfo:
                         }
                     )
 
-            # 更新bug的类型数
             bug_stats["bug类型"] = len(bug_types)
-            # 将统计结果合并到报告数据中
             report_data.update(bug_stats)
             report_data.update({"bug类型": bug_types})
-            ReportStyle.step("BUG数据总览: ", bug_stats)
+            # ReportStyle.step("BUG数据总览: ", bug_stats)
 
         with allure.step("step: 断言bug的类型与定位是否一致"):
             base_bugs = args["bugs"]
             with assume:
                 test_bugs = None if not test_bugs else test_bugs
                 if base_bugs is not None and test_bugs is not None:
-                    # 按照 'name' 和 'location' 键排序
                     sorted_base_bugs = sorted(base_bugs, key=lambda x: (x['name'], x['location']))
                     sorted_test_bugs = sorted(test_bugs, key=lambda x: (x['name'], x['location']))
                     assert sorted_base_bugs == sorted_test_bugs
-                    ReportStyle.step("bug的类型与定位", test_bugs)
+                    # ReportStyle.step("bug的类型与定位", test_bugs)
                 else:
                     assert base_bugs == test_bugs
-                    ReportStyle.step("bug的类型与定位", None)
+                    # ReportStyle.step("bug的类型与定位", None)
+
+    import os
+
+    def write_report(self, report_data):
+        # print(report_data)
+        elements = list(report_data["bug类型"])
+        separator = ', '
+        result_string = separator.join(elements)
+        report = [report_data["用例名"], report_data["代码行"], report_data["函数个数"], report_data["gtest行覆盖"],
+                report_data["gtest函数覆盖"], report_data["fuzzer行覆盖"], report_data["fuzzer函数覆盖"],
+                report_data["gtest行覆盖率"], report_data["gtest函数覆盖率"], report_data["fuzzer行覆盖率"], report_data["fuzzer函数覆盖率"],
+                report_data["行覆盖提升率"], report_data["函数覆盖提升率"], report_data["bug总数"], result_string, report_data["用例bug数"],
+                report_data["源码bug数"]]
+        print(report)
+        generate_report(report)
+        # import csv
+        # from datetime import datetime
+        # today = datetime.today().strftime('%Y-%m-%d')
+        # filename = f"{today}.csv"
+        #
+        # file_exists = os.path.isfile(filename)
+        #
+        # with open(filename, 'a', newline='', encoding='utf-8-sig') as csvfile:
+        #     fieldnames = report_data.keys()
+        #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        #
+        #     if not file_exists:
+        #         writer.writeheader()
+        #
+        #     writer.writerow(report_data)
+        #
+        # print(f"数据已保存到文件 {filename}")
+
+    @pytest.mark.parametrize("args", test_cases)
+    def test_run_all(self, args, report_data):
+        self.gather_coverage_info(args, report_data)
+        self.gather_bugs_info(args, report_data)
+        self.write_report(report_data)
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", __file__])
+
+
 
